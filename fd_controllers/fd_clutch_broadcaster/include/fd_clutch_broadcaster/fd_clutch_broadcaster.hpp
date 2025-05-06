af8a766088d052e78d0e9dd63a74e83ae1389ae7
Chen Chen <maker_cc@foxmail.com>
2025-05-06 20:24:32 +0800

    Use realtime_publisher.hpp (#33)
    * Update ee_pose_broadcaster.hpp
    
    * Update fd_clutch_broadcaster.hpp

--- fd_controllers/fd_clutch_broadcaster/include/fd_clutch_broadcaster/fd_clutch_broadcaster.hpp
+++ fd_controllers/fd_clutch_broadcaster/include/fd_clutch_broadcaster/fd_clutch_broadcaster.hpp
@@ -28,7 +28,7 @@
 #include "controller_interface/controller_interface.hpp"
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
-#include "realtime_tools/realtime_publisher.h"
+#include "realtime_tools/realtime_publisher.hpp"
 
 #include "std_msgs/msg/bool.hpp"
 
